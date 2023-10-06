4daa58dac15204aad2624f81277361aab95a2925
victor <ifeanyivictortech@gmail.com>
2023-10-06 14:00:03 -0700

    a programme that uses puts statement to print something

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*Main - This Prints to the console
+*
+*Description: This code prints what C language is like
+*
+*Return: 0 after being successful
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle.");
+	return (0);
+}
