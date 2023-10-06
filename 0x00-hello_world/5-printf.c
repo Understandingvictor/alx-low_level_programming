fef9425d5fe5508ae013054230ea8d6f595796c3
victor <ifeanyivictortech@gmail.com>
2023-10-06 15:06:11 -0700

    A C program that prints to the console and adds a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - This program prints to the console or stdout
+*description - uses some set boundary to print
+*Return - Returns 0 on success
+*/
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
