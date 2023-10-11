9e2a5b9f077c02e53fd545fa2d296724c5415cc7
victor <ifeanyivictortech@gmail.com>
2023-10-11 06:05:37 -0700

    A program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/**
+*main - prints _putchar
+*Return: 0 upon completion
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+The files contained within are for the 0x02-functions_nested_loops
