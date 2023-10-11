63b7bd76bc3e87cce0aff0f759c65734d5771b9e
victor <ifeanyivictortech@gmail.com>
2023-10-11 09:38:04 -0700

    this program prints _putchar using function declared in a separate header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,20 @@
 *Return: 0 upon completion
 */
 
-#include <stdio.h>
+#include "main.h"
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	printf("_putchar\n");
 	return (0);
 }
+
