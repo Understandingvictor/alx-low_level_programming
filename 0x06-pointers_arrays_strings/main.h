9edaefbf9df3bcefc31aa7157f86aca1557f6a24
victor <ifeanyivictortech@gmail.com>
2023-10-21 12:27:18 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+#endif
