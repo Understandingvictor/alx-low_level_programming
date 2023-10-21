7a268813a1860dd9ca95640ec29a34319e042261
victor <ifeanyivictortech@gmail.com>
2023-10-21 13:29:02 -0700

    a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+*_strcmp - a function that compares two strings.
+*@s1: parameter to be taken
+*@s2: parameter to be taken
+*Return: 0 upon completion
+*/
+int _strcmp(const char *s1, const char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+
+	return ((*s1 > *s2) ? 1 : -1);
+}
