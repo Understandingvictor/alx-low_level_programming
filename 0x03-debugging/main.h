8b7ae3168fa32f3da2dd50c64f1e064a5ba15c70
ifeanyi victor <victorflawless7@gmail.com>
2023-11-02 11:48:27 +0100

    program converts a date to the day of year and determines how many days are left in the year, taking leap year into consideration.

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-int largest_number(int a, int b, int c)
+void print_remaining_days(int month, int day, int year);
+int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
 #endif
