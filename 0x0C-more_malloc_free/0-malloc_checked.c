c072edbc3f40ddea5d7f407d7b78644e73d065b3
Understandingvictor <victorflawless7@gmailcom>
2024-01-18 14:11:35 +0100

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+/**
+  *malloc_checked - allocates memory using malloc
+  *@b: size
+*Return: a memory address
+*/
+
+#include <stdlib.h>
+#include "main.h"
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is project for more_malloc_free project 0x0c
