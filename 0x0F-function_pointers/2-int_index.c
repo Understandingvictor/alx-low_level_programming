0b01001994ca68fdc5477fb42b8a1c5e748edba0
Understandingvictor <victorflawless7@gmailcom>
2024-01-24 18:37:03 +0100

    added some file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+/**
+  *int_index - serches for an integer
+  *@array: parameter to be passed
+  *@size: size of array
+  *@cmp: pointer to a function
+  *Return: 0
+  */
+
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]))
+		{
+			return (i);
+		}
+	}
+		return (-1);
+}
