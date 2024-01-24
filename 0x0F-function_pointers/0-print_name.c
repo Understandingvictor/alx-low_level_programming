a508f2827d73f6b2ba919d7b7de5dcbe3133539a
Understandingvictor <victorflawless7@gmailcom>
2024-01-24 15:50:15 +0100

    function that prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,6 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
-  *functionPtr - prints a name
-  *@name: parameter to take in
-  *Return: nothing on completion
-  */
-
-void functionPtr(char *name)
-{
-	printf("%s\n", name);
-}
-
-
-/**
   *print_name - prints a name
   *@name: parameter
   *@f: pointer to a funtion
@@ -21,5 +9,6 @@ void functionPtr(char *name)
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name != NULL && f != NULL)
+		(*f)(name);
 }
