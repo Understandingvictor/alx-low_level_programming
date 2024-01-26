9c5b915cbdfc271eae89cbf057af38e6eb31cd65
Understandingvictor <victorflawless7@gmailcom>
2024-01-26 16:50:57 +0100

    function that prints numbers while printing strings between numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,20 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arguments;
 
-	if (separator == NULL)
-		exit(1);
-
 	va_start(arguments, n);
-
 	for (i = 0; i < n; i++)
 	{
-		if (i == 0)
+		if (!separator)
+			printf("%d", va_arg(arguments, int));
+		else if (separator && i == 0)
 			printf("%d", va_arg(arguments, int));
 		else
 			printf("%s%d", separator, va_arg(arguments, int));
-
-		if (i == n - 1)
-			break;
 	}
 	va_end(arguments);
 	printf("\n");
