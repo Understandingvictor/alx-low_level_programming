ac8d112f0d9b3d2f6dd17dd3b665b75ce6bba2e1
Understandingvictor <victorflawless7@gmailcom>
2024-02-02 15:38:33 +0100

    signly list functions. description in each

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,17 +10,16 @@ size_t list_len(const list_t *h)
 {
 	unsigned int count = 0;
 	
-	if (h == NULL)
-		return (NULL);
-	
 	
 		if (h->str != NULL)
+		{
 			count++;
+		}
 
 		if (h->len)
+		{
 			count++;
-
-		h = h->next;
+		}
 
 	return (count);
 }
